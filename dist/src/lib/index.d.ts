6f4855afd576e935531e27bc912e1094fd507d4d
Justin <justin@homeceuconnection.com>
2018-03-28 14:35:55 -0500

    removed unused files.

--- dist/src/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { PromptComponent } from './Component/PromptComponent';
-export { PromptBodyComponent } from './Component/PromptBodyComponent';
-export { PromptHeaderComponent } from './Component/PromptHeaderComponent';
-export { PromptConfirmComponent } from './Component/PromptConfirmComponent';
-export { PromptCancelComponent } from './Component/PromptCancelComponent';
-export { PromptModalComponent } from './Component/PromptModalComponent';
-export { NgPromptModule } from "./NgPromptModule";
--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Module: Example', () => {
-    describe('Class: Example Class', () => {
-        describe('Method: Example Method', () => {
-            it('should execute something', () => {
-                // do something
-            })
-        })
-    })
-});
